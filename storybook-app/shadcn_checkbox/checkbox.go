c6fc4f0f44692f60f6b9bfa2b1364b82dd19d247
Benjamin Gudehus <hastebrot@gmail.com>
2024-07-07 03:11:37 +0200

    feature: shadcn checkbox added. (#11)

---
+++ storybook-app/shadcn_checkbox/checkbox.go
@@ -0,0 +1,63 @@
+package shadcn_checkbox
+
+import (
+	"github.com/hastebrot/gomponents-lucide/lucide"
+	g "github.com/maragudk/gomponents"
+	. "github.com/maragudk/gomponents/html"
+
+	. "storybook-app/helper"
+)
+
+type CheckboxProps struct {
+	IsChecked bool
+}
+
+func Checkbox(props CheckboxProps) g.Node {
+	return Div(Class("flex items-center space-x-2"),
+		Button(
+			Role("checkbox"),
+			Classes(
+				Class("peer h-4 w-4 shrink-0 rounded-[4px] border border-[#18181B]"),
+				Class("focus-visible:outline-none"),
+				Class("disabled:cursor-not-allowed disabled:opacity-50"),
+				g.If(props.IsChecked, Class("bg-[#18181B] text-[#FAFAFA]")),
+			),
+			g.If(props.IsChecked,
+				Span(Class("flex items-center justify-center text-current"),
+					lucide.Check(Class("block h-3.5 w-3.5 flex-shrink-0 [stroke-width:2px]")),
+				),
+			),
+		),
+		CheckboxLabel(CheckboxLabelProps{
+			Text: "Label",
+		}),
+	)
+}
+
+type CheckboxLabelProps struct {
+	Text string
+}
+
+func CheckboxLabel(props CheckboxLabelProps) g.Node {
+	return Label(Class("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"),
+		g.Text(props.Text),
+	)
+}
+
+func StoryCheckbox() g.Node {
+	return Div(
+		story(
+			Checkbox(CheckboxProps{}),
+			Checkbox(CheckboxProps{
+				IsChecked: true,
+			}),
+		),
+	)
+}
+
+func story(children ...g.Node) g.Node {
+	return Div(
+		Class("flex min-h-[150px] w-full items-start justify-center p-10 gap-10"),
+		g.Group(children),
+	)
+}
